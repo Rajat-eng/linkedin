fb39232a746c67aabf795ab2e96fb1f9a0fe5d93
Rajat-Imme <rajat.verma@immersivelabz.com>
2025-04-01 18:53:13 +0530

    4

--- posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
+++ posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
@@ -1,19 +1,25 @@
 package com.codingshuttle.linkedin.posts_service.controller;
 
-import com.codingshuttle.linkedin.posts_service.dto.PostCreateRequestDto;
-import com.codingshuttle.linkedin.posts_service.dto.PostDto;
-import com.codingshuttle.linkedin.posts_service.entity.Post;
-import com.codingshuttle.linkedin.posts_service.service.PostsService;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingshuttle.linkedin.posts_service.dto.PostCreateRequestDto;
+import com.codingshuttle.linkedin.posts_service.dto.PostDto;
+import com.codingshuttle.linkedin.posts_service.service.PostsService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/core")
 @RequiredArgsConstructor
 public class PostsController {
 
